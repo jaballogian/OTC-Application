a5fe8242ce14cec022957aaf04252e9acc3919ce
jaballogian <jaballogianrma77@gmail.com>
2020-05-03 17:22:51 +0700

    finish activity welcome screen and add new activity (activity home)

--- app/src/main/java/com/otc/application/ActivityWelcomeScreen.java
+++ app/src/main/java/com/otc/application/ActivityWelcomeScreen.java
@@ -2,13 +2,36 @@ package com.otc.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class ActivityWelcomeScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMED_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+
+                goToMainAcitivty();
+            }
+        },SPLASH_TIMED_OUT);
+    }
+
+    private void goToMainAcitivty(){
+        Intent toActivityMain = new Intent(ActivityWelcomeScreen.this, ActivityHome.class);
+        toActivityMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(toActivityMain);
+        finish();
     }
 }
