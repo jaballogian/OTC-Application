ddade57bd31c6bafd836972e45a20395a0025368
jaballogian <jaballogianrma77@gmail.com>
2020-05-10 03:47:08 +0700

    add user registration in activity sign up

--- app/src/main/java/com/otc/application/activity/ActivitySignUp.java
+++ app/src/main/java/com/otc/application/activity/ActivitySignUp.java
@@ -1,7 +1,9 @@
 package com.otc.application.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,7 +18,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.otc.application.encryptionanddecryption.EncryptionAndDecryption;
@@ -38,6 +45,10 @@ public class ActivitySignUp extends AppCompatActivity {
     private HashMap<String, EditText> userDataHashMapEditText;
     private ArrayList<Spinner> arrayListSpinner;
     private CheckBox checkBoxPersetujuanPendaftaran;
+    private ProgressDialog loading;
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    private String uID;
 
     public static DatabaseReference bidangReference, bidangSDReference, bidangSMPReference, bidangSMAReference;
     public static AppCompatSpinner spinnerBidang;
@@ -48,6 +59,10 @@ public class ActivitySignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         commonMethods = new CommonMethods(this);
+
+        loading = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
 
         halamanPendaftaranReference = FirebaseDatabase.getInstance().getReference().child("Halaman").child("Pendaftaran");
         jenisAkunReference = halamanPendaftaranReference.child("Jenis Akun");
@@ -149,8 +164,9 @@ public class ActivitySignUp extends AppCompatActivity {
 
                 if(userDataFromSpinner.get(getString(R.string.jenis_akun)).equals(getString(R.string.jenis_akun))
                         || userDataFromSpinner.get(getString(R.string.jenjang_sekolah)).equals(getString(R.string.jenjang_sekolah))
-                        || userDataFromSpinner.get(getString(R.string.tipe_sekolah)).equals(getString(R.string.tipe_sekolah))){
-                    Toast.makeText(ActivitySignUp.this, getString(R.string.pilih_jenis_akun_dan_jenjang_akun_yang_valid), Toast.LENGTH_LONG).show();
+                        || userDataFromSpinner.get(getString(R.string.tipe_sekolah)).equals(getString(R.string.tipe_sekolah))
+                        || userDataFromSpinner.get(getString(R.string.bidang)).equals(getString(R.string.bidang))){
+                    Toast.makeText(ActivitySignUp.this, getString(R.string.pilih_jenis_akun_jenjang_akun_dan_bidang_yang_valid), Toast.LENGTH_LONG).show();
                 }
                 else {
                     userData.putAll(userDataFromEditText);
@@ -159,9 +175,8 @@ public class ActivitySignUp extends AppCompatActivity {
                     userData.put(getString(R.string.kata_sandi), EncryptionAndDecryption.encrypt(userData.get(getString(R.string.kata_sandi))));
                     Log.d("userData", userData.toString());
 
-                    //TODO: register user here
-
-//                    moveToActivityHome();
+                    setProgressDialog();
+                    registerUser(userData);
                 }
             }
         }
@@ -181,5 +196,47 @@ public class ActivitySignUp extends AppCompatActivity {
         Intent toActivitySignIn = new Intent(ActivitySignUp.this, ActivitySignIn.class);
         startActivity(toActivitySignIn);
         finish();
+    }
+
+    private void registerUser(final HashMap<String, String> inputHashMap){
+        mAuth.createUserWithEmailAndPassword(inputHashMap.get(getString(R.string.email)), inputHashMap.get(getString(R.string.kata_sandi))).
+                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    saveUserToDatabase(inputHashMap);
+                    loading.dismiss();
+                }
+                else {
+                    loading.hide();
+                    Toast.makeText(ActivitySignUp.this, R.string.pilih_email_dan_password_yang_berbeda, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void saveUserToDatabase (HashMap<String, String> inputHashMap){
+        currentUser = mAuth.getCurrentUser();
+        uID = currentUser.getUid();
+
+        DatabaseReference profileReference = FirebaseDatabase.getInstance().getReference().child("Sementara").child("Pengguna").child(uID).child("Profil");
+        profileReference.setValue(inputHashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if(task.isSuccessful()){
+                    moveToActivityHome();
+                    Toast.makeText(ActivitySignUp.this, getString(R.string.selamat_datang), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void setProgressDialog(){
+
+        loading.setTitle(R.string.sedang_memproses);
+        loading.setMessage(getString(R.string.mohon_tunggu));
+        loading.setCanceledOnTouchOutside(false);
+        loading.show();
     }
 }
